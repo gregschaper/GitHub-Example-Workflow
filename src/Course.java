912e870e01424a1518a473591b9b9389dd6f7009
Trevor Gray <trevormgray@gmail.com>
2024-02-08 10:37:36 -0500

    added team

--- src/Course.java
+++ src/Course.java
@@ -29,6 +29,7 @@ public class Course
     {
         teams.add(new TeamCool("TeamCool"));
         teams.add(new TeamNice("TeamNice"));
+        teams.add(new TeamAwesome("Team6"));
         // create each new team and add to team list;
         // Repository owner will do this.
     }
