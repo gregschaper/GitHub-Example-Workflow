10d65e2a3785dd1dad6424ce1cf5fd9881f3aa04
Jesse Russo <russoj03@moravian.edu>
2024-02-08 10:44:58 -0500

    Added Project00

---
+++ Project00/Course.java
@@ -0,0 +1,38 @@
+package Project00;
+
+import java.util.ArrayList;
+import Project00.TeamCool;
+import Project00.Member;
+
+public class Course
+{
+    ArrayList<Team> teams = new ArrayList<>();
+
+    public Course()
+    {
+
+    }
+
+
+    public void printCourseMembers()
+    {
+        for(int i = 0; i < teams.size(); i++)
+        {
+            System.out.println(teams.get(i).getTeamName());
+            teams.get(i).printTeamMembers();
+            System.out.println("\n");
+        }
+    }
+
+
+    public void createTeams()
+    {
+        teams.add(new TeamCool("TeamCool"));
+        teams.add(new TeamNice("TeamNice"));
+        teams.add(new Team6("Team6"));
+        // create each new team and add to team list;
+        // Repository owner will do this.
+    }
+
+}
+
